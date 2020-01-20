397d2fbeddc17e93d54a67b62173001861c44ea8
shrikumaran <51413553+shrikumaran@users.noreply.github.com>
2020-01-20 23:06:43 +0530

    Add files via upload

---
+++ pytorch test.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import torch
+import numpy
+
+# let us run this cell only if CUDA is available
+# We will use ``torch.device`` objects to move tensors in and out of GPU
+x = torch.randn(4, 4)
+if torch.cuda.is_available():
+    device = torch.device("cuda")          # a CUDA device object
+    y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
+    x = x.to(device)                       # or just use strings ``.to("cuda")``
+    z = x + y
+    print(z)
+    print(z.to("cpu", torch.double))       # ``.to`` can also change dtype together!
